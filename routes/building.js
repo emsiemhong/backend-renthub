290c21e97c95c45c50cf437444469430c621acf0
SIEMHONG <webridge@WEBRIDGEs-MacBook-Pro.local>
2020-07-04 10:09:28 +0700

    fix

--- routes/building.js
+++ routes/building.js
@@ -1,10 +1,15 @@
 var express = require("express");
-const buildingController = require("../controllers/buildingController");
+const apiResponse = require("../helpers/apiResponse");
+const Building = require("../models/Building");
+//const buildingController = require("../controllers/buildingController");
 
 var router = express.Router();
-
-router.get("/", buildingController.getBuildings);
-router.get("/:id", buildingController.getBuilding);
+router.get("/", async(req, res) => {
+    const buildings = await Building.find();
+    return apiResponse.successResponseWithData(res, "Success", buildings);
+});
+//router.get("/", buildingController.getBuildings);
+//router.get("/:id", buildingController.getBuilding);
 // router.post("/", buildingController.buildingStore);
 // router.put("/:id", buildingController.buildingUpdate);
 // router.delete("/:id", buildingController.buildingDelete);
